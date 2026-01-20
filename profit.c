8302327c18068daea51592ac7361563e3662914b
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-01-20 10:14:10 -0300

    Implement maxProfit function to calculate profit

---
+++ profit.c
@@ -0,0 +1,18 @@
+int maxProfit(int* prices, int pricesSize) {
+    int min_price = prices[0];
+    int max_profit = 0;
+    int i;
+
+    for (i = 1; i < pricesSize; i++) {
+        if (prices[i] < min_price) {
+            min_price = prices[i];
+        } else {
+            int profit = prices[i] - min_price;
+            if (profit > max_profit) {
+                max_profit = profit;
+            }
+        }
+    }
+
+    return max_profit;
+}
