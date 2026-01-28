9765bac09353bc51b6d9e5bddabfaa81681c1bdb
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-01-28 14:05:46 -0300

    Create PivotIndexChar.c

---
+++ PivotIndexChar.c
@@ -0,0 +1,27 @@
+int pivotStringIndex(char* s) {
+    int n = strlen(s);
+    int i;
+    for (i = 0; i < n; i++) {
+       
+      
+        int comp_esq = i;
+        int comp_dir = n - i - 1;
+        if (comp_esq != comp_dir) {
+         
+            continue;
+        }
+       
+        int j;
+        int igual = 1;
+        for (j = 0; j < comp_esq; j++) {
+            if (s[j] != s[i+1+j]) {
+                igual = 0;
+                break;
+            }
+        }
+        if (igual) {
+            return i;
+        }
+    }
+    return -1;
+}
