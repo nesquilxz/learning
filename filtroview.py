7629a2ad9acdd080336646d778781023ee8feab2
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-02-09 09:58:24 -0300

    Add filtroview.py for filtering clientes data

---
+++ filtroview.py
@@ -0,0 +1,15 @@
+# %%
+import pandas as pd
+
+clientes = pd.read_csv("../data/clientes.csv", sep=";")
+clientes
+# %%
+
+filtro = clientes["qtdePontos"] == 0
+clientes_0 = clientes[filtro]
+clientes_0
+# %%
+clientes_0["flag_1"] = 1
+# %%
+clientes_0
+# %%
