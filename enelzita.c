6ac92a0c3dfbdfc18fbecd85c02972ade23a502e
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-03-16 17:57:41 -0300

    Update enelzita.c

--- enelzita.c
+++ enelzita.c
@@ -1,34 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-
-int main (void) {
-
-    char nome[100];
+    #include <stdio.h>
 
 
-    printf("Digite seu nome: \n");
-    scanf("%99s", nome);
+    int main (void) {
+
+        char nome[100];
 
 
-    int idade;
-    scanf("%d", &idade);
+        printf("Digite seu nome: ");
+        scanf("%99s", nome);
 
-    float salarioMin;
-    printf("Digite o salario minimo: \n");
-    scanf("%f", &salarioMin);
+        float salarioMin;
+        printf("Digite o salario minimo: \n");
+        scanf("%f", &salarioMin);
 
-    float qtdKw;
-    printf("Digite a quantidade de KiloWatts gasto pelo o usuário (%s): \n", nome);
-    scanf("%f", &qtdKw);
+        float qtdKw;
+        printf("Digite a quantidade de KiloWatts gasto pelo o usuário (%s): \n", nome);
+        scanf("%f", &qtdKw);
 
-    float valorKw = salarioMin * 0.01;
+        float valorKw = salarioMin * 0.01;
 
-    printf("O valor do Kw é de: %f \n", valorKw);
+        printf("O valor do Kw é de: %f \n", valorKw);
 
-    float pagar = valorKw*qtdKw;
+        float pagar = valorKw*qtdKw;
 
-    printf("O valor a ser pago este mes e': %.2f \n", pagar);
+        printf("O valor a ser pago este mes e': %.2f \n", pagar);
 
 
 
-}
+    }
