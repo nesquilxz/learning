7aa1714348be04e1f30a534a1e71a4139fb939dd
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-04-16 10:57:58 -0300

    Add matriculas2.c for student registration and checks

---
+++ matriculas2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void preencheturma(int *v, int n) {
+
+    for (int i =0; i < n; i++) {
+
+        printf("Digite a matricula do aluno numero %d:  ", i+1);
+        scanf("%d", &v[i]);
+    }
+
+
+}
+
+
+void verificairregulares(int *v1, int *v2, int n1, int n2) {
+
+    for(int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (v1[i] == v2[j]) {
+                printf("Aluno com a matricula: %d esta irregular!", v1[i]);
+            }
+        }
+    }
+
+
+
+}
+
+
+
+int main(void) {
+
+    int N, M, i;
+
+    int *prog1, *prog2;
+
+
+    printf("Digite a quantidade de alunos de Prog 1: ");
+    scanf("%d", N);
+    prog1 = (int *) malloc(sizeof(int) * N);
+
+
+    printf("Digite a quantidade de alunos de Prog 2: ");
+    scanf("%d", M);
+    prog2 = (int *) malloc(sizeof(int) * M);
+
+
+    printf("\n Digite as matrículas dos alunos de prog1\n");
+    preenchevetor(prog1, N);
+
+
+    printf("\n Digite as matrículas dos alunos de prog2\n");
+    preenchevetor(prog2, M);
+
+    verificairregulares(prog1, prog2, N, M);
+}
