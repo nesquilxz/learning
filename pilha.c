ee01ac1a3f2837e078bd622a976e8628dd852b50
Pedro Henriques <pedrohenriquesuff@gmail.com>
2026-08-11 12:42:22 -0300

    Add stack implementation in pilha.c
    Implement stack operations including push and pop.

---
+++ pilha.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct no
+{
+    int info;
+    struct lista *prox;
+} Tpilha;
+
+
+
+Tpilha* pull(Tpilha* topo, int elem) {
+
+    Tpilha *novo = malloc(sizeof(Tpilha));
+    novo->info = elem;
+    novo->prox = topo;
+    topo = novo;
+    return novo;
+
+}
+
+
+Tpilha* pop(Tpilha *topo) {
+
+    Tpilha*aux = (Tpilha*) malloc(sizeof(Tpilha));
+    aux->info = topo->info;
+
+    Tpilha *lixo = topo;
+    topo = topo->prox;
+
+    free(lixo);
+
+    return aux;
+
+}
+
+
+
+int main(void) {
+
+    Tpilha *topo;
+    topo = NULL;
+
+    // . . . testar funções
+}
+
